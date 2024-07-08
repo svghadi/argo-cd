8e2f9ce3bc1251e890c26dc0a597dbb0282b5348
Siddhesh Ghadi <sghadi1203@gmail.com>
2024-07-08 18:14:29 +0530

    Return map instead of slice for annotations
    Signed-off-by: Siddhesh Ghadi <sghadi1203@gmail.com>

--- util/settings/settings_test.go
+++ util/settings/settings_test.go
@@ -1779,24 +1779,24 @@ func TestSettingsManager_GetHideSecretAnnotations(t *testing.T) {
 	tests := []struct {
 		name   string
 		input  string
-		output []string
+		output map[string]bool
 	}{
 		{
 			name:   "Empty input",
 			input:  "",
-			output: []string{},
+			output: map[string]bool{},
 		},
 		{
 			name: "Correct format",
 			input: `- example.com/token-secret.value
 							- token`,
-			output: []string{"example.com/token-secret.value", "token"},
+			output: map[string]bool{"example.com/token-secret.value": true, "token": true},
 		},
 		{
 			name: "Partially correct format with each key on new line - returns correct keys",
 			input: `  example.com/token-secret.value  
 							-token`,
-			output: []string{"example.com/token-secret.value", "token"},
+			output: map[string]bool{"example.com/token-secret.value": true, "token": true},
 		},
 	}
 	for _, tt := range tests {
@@ -1806,10 +1806,7 @@ func TestSettingsManager_GetHideSecretAnnotations(t *testing.T) {
 			})
 			keys := settingsManager.GetHideSecretAnnotations()
 			assert.Equal(t, len(tt.output), len(keys))
-
-			for i := range tt.output {
-				assert.Equal(t, tt.output[i], keys[i])
-			}
+			assert.Equal(t, tt.output, keys)
 		})
 	}
 }
